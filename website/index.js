c0958844667033d5ca638a496042cfda2b4b2fe1
Your Name <wjdanjo2@outlook.sa>
2021-03-12 20:27:52 +0300

    true

--- website/index.js
+++ website/index.js
@@ -1,9 +1,6 @@
-/* Global Variables */
 import Check_display from './submit.js'
 const Url = "http://api.openweathermap.org/data/2.5/weather?zip=";
-
 const api_Key = "&appid=d962862dddca2d0427bb9f01a032cd06";
-
 const submit_Btn = document.querySelector('#generate')
 
 let d_ = new Date();
@@ -11,16 +8,10 @@ let new_Date = d_.getMonth() + '.' + d_.getDate() + '.' + d_.getFullYear();
 
 submit_Btn.addEventListener('click', (w) => {
     w.preventDefault();
-    console.log("EventListen its work");
-    const zipCode = document.getElementById('zip').value; // take input value 
-    const tempr = document.querySelector('#temp');
-    const country_name = document.querySelector('#name');
-    const Country = document.querySelector('#country');
-    const data_Time = document.querySelector('#date');
-    const Weather = document.querySelector('#weather');
-    const feelings = document.querySelector('#feel');
-    const textArea = document.getElementById('feelings').value; // take input value
-   
+    console.log("Event work");
+    const zipCode = document.getElementById('zip').value; 
+    const textArea = document.getElementById('feelings').value; 
+
      getData(Url, zipCode, api_Key) //return resData
      
     .then((resData) =>{
@@ -31,27 +22,12 @@ submit_Btn.addEventListener('click', (w) => {
         Country.textContent=  resData.sys.country 
         tempr.textContent=resData.main.temp
         Weather.textContent= resData.weather[0].main
-        
-        
     })
 
-    Check_display()
+    Check_display(zipCode)
 }); 
 
 
-// function Check_display (){
-   
-//     const input = document.querySelector('.input');
-//     if(input.value === ''){
-//     alert('Please Enter Zip Code For (USA) Only, EX : 90232  ')
-    
-//     }  
-//     else if(submit_Btn.click=true){
-//             console.log("fuck u")
-//             const list_info = document.querySelector('.entry')
-//             list_info.style.display = 'block'
-//         }
-// }
 async function getData(url, zipCode, api_Key) {
     console.log('start getting data')
         // bring all data and the input value 
--- website/submit.js
+++ website/submit.js
@@ -1,15 +1,15 @@
-function Check_display (){
-    const submit_Btn = document.querySelector('#generate')
-    
-    const input = document.querySelector('.input');
-    if(input.value === '' ){
-    alert('Please Enter Zip Code For (USA) Only, EX : 90232  ')
-    
-    }  
-    else if(submit_Btn.click=true){
-            console.log("fuck u")
-            const list_info = document.querySelector('.entry')
-            list_info.style.display = 'block'
-        }
+function Check_display (code){
+    var zipCodePattern = /^\d{5}$|^\d{5}-\d{4}$/;
+    if( zipCodePattern.test(code)){
+        console.log('ok yes')
+          const list_info = document.querySelector('.entry')
+          list_info.style.display = 'block'
+    }else {
+     alert('invalid zip code (USA) Only, EX : 90232  ')  
+    }
+
+  
+
 }
 export default Check_display;
+    
